fcd95cd42a08e94fd0aac95351beaa6edd2bf96f
Pere Solà Claver <pere.solaclaver@gmail.com>
2024-11-03 15:52:51 +0000

    Create sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()],
+  site: import.meta.env.URL,
+  integrations: [tailwind(), partytown(), sitemap()],
 });
